1ad4bd09f3154a2f074adbd0fdaa0bdd36455d22
Lucas Fraga Mendes <lucasfragamendes@gmail.com>
2026-08-22 09:41:18 -0300

    adding if and w3

--- processaCadastro.php
+++ processaCadastro.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
     <title>Ação</title>
 </head>
 <body class="fundo">
@@ -42,8 +43,14 @@
     </div>
     <div class="fontes">
     <?php
-        echo "Obrigado pelo cadastro, $nome! Seu interesse na vaga de $profissao foi registrado. Sua experiência informada foi: $experiencia.";
+        if($idade >= 18){
+            echo "Obrigado pelo cadastro, $nome! Seu interesse na vaga de $profissao foi registrado. Sua experiência informada foi: $experiencia.";;
+        }
+        else{
+           echo "Você não tem idade suficiente para se candidatar a esta vaga.";
+        }
     ?>
+    </div>
     </div>
     <input name="Voltar" type="submit" id="voltar" value="Voltar" onclick="window.location.href='request.html'">
 </body>
